80a777b26fa5a6be79d3fc821581be3f66aba25d
Arlin Grijalba <arlin.grijalba@gmail.com>
2018-03-06 12:18:50 -0800

    booleans

---
+++ LearningPython/booleans.py
@@ -0,0 +1,34 @@
+# Python booleans
+
+python_course = True
+java_course = False
+
+print(python_course)
+
+print()
+
+# True is 1
+print("Is this a python course?")
+if python_course == 1:
+    print(int(python_course))
+else:
+    if python_course == 0:
+        # False is 0
+        print(int(python_course))
+
+print()
+
+# False is 0
+print("Is this a java course?")
+if java_course == 1:
+    print(int(java_course))
+else:
+    if java_course == 0:
+        # False is 0
+        print(int(java_course))
+
+print()
+
+# NONE value
+number_of_automation_test_by_me = None
+print(number_of_automation_test_by_me)
