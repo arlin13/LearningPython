98a23dc87cbd43b1eb28209e0eeac6b891790535
Arlin Grijalba <arlin.grijalba@gmail.com>
2018-03-09 09:40:28 -0800

    yield

---
+++ LearningPython/yield.py
@@ -0,0 +1,29 @@
+"""
+YIELD
+Used to pass/return (yield) value to caller function with every iteration
+"""
+
+
+def read_file():
+    try:
+        file = open("yield.txt", "r")
+        # for line in file.readlines():
+        for line in read_line(file):
+            print(line)
+        file.close()
+    except Exception as error:
+        print(f"Could not read file: {error}")
+
+
+# returns every line of file
+def read_line(file):
+    for line in file:
+        yield line
+
+# returns only first line: 'hola'
+# def read_line(file):
+#     for line in file:
+#         return line
+
+
+read_file()
